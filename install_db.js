f1170cdf64e82ff42b1e8e6c9fbfb3c30b106db8
Angel Calvo <calvo.monzon.angel@gmail.com>
2021-09-14 18:42:57 +0200

    insercción imagenes

--- install_db.js
+++ install_db.js
@@ -4,4 +4,20 @@ const database = require("./lib/connectMongoose");
 const Anuncio = require("./models/Anuncio");
 const anunciosData = require("./data/anuncios.json");
 
-database.once
+main().catch(err => console.log('Hubo un error', err));
+
+async function main() {
+  await iniciarAnuncios();
+
+  database.close();
+}
+
+async function iniciarAnuncios() {
+  // elimino todos los anuncios existentes
+  const deleted = await Anuncio.deleteMany();
+  console.log(`Eliminados ${deleted.deletedCount} anuncios.`);
+
+  // creo los anuncios iniciales
+  const anuncios = await Anuncio.insertMany(anunciosData.anuncios);
+  console.log(`Creados ${anuncios.length} anuncios.`);
+};
