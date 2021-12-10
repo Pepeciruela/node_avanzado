dd10dcde6e30cad24c798c43a0750bfc695bf4cd
Angel Calvo <calvo.monzon.angel@gmail.com>
2021-12-10 16:29:27 +0100

    cambio repo

--- install_db.js
+++ install_db.js
@@ -1,18 +1,40 @@
 "use strict"
 
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const readline = require('readline');
 const database = require("./lib/connectMongoose");
-const Anuncio = require("./models/Anuncio");
 const anunciosData = require("./data/anuncios.json");
 
 main().catch(err => console.log('Hubo un error', err));
 
-async function main() {
-  await iniciarAnuncios();
+const {Anuncio, Usuario} = require('./models');
 
-  database.close();
+async function main() {
+
+  await new Promise((resolve, reject) => {
+    database.once('open', resolve);
+    database.once('error', reject);
+  });
+
+  if (!(await askYesNo('Estas seguro que quieres inicializar la BD? (yes/no)'))) {
+    console.log('Abortado el comando. No se ha borrado nada.');
+    return process.exit(0);
+  }
+
+  // inicializo la colección de anuncios
+  await initAnuncios();
+
+  // inicializo la colección de usuarios
+  await initUsuarios();
+
+  mongoose.connection.close();
+
 }
 
-async function iniciarAnuncios() {
+
+async function initAnuncios() {
   // elimino todos los anuncios existentes
   const deleted = await Anuncio.deleteMany();
   console.log(`Eliminados ${deleted.deletedCount} anuncios.`);
@@ -21,3 +43,38 @@ async function iniciarAnuncios() {
   const anuncios = await Anuncio.insertMany(anunciosData.anuncios);
   console.log(`Creados ${anuncios.length} anuncios.`);
 };
+
+async function initUsuarios() {
+
+  await Usuario.deleteMany();
+  console.log('Usuarios borrados.');
+
+  const numLoaded = await Usuario.insertMany([
+    {
+      email: "user@example.com",
+      password: await Usuario.hashPassword("1234")
+    }
+  ]);
+  console.log(`Se han cargado ${numLoaded.length} usuarios.`);
+
+  return numLoaded;
+
+};
+
+function askYesNo(questionText) {
+  return new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question(questionText, answer => {
+      rl.close();
+      if (answer.toLowerCase() === 'yes') {
+        resolve(true);
+        return;
+      }
+      resolve(false);
+    })
+  });
+}
+
